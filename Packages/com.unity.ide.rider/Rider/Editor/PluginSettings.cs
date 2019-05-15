f217b80abf3313be6494ba08044c3497c39fbb0a
Ivan.Shakhov <van800@gmail.com>
2019-05-15 15:56:28 +0200

    convert PreferenceItem into SettingsProvider

--- Packages/com.unity.ide.rider/Rider/Editor/PluginSettings.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/PluginSettings.cs
@@ -131,111 +131,125 @@ namespace Packages.Rider.Editor
       margin = new RectOffset(4, 4, 4, 4),
     };
 
-
     /// <summary>
     /// Preferences menu layout
     /// </summary>
     /// <remarks>
     /// Contains all 3 toggles: Enable/Disable; Debug On/Off; Writing Launch File On/Off
     /// </remarks>
-    [PreferenceItem("Rider")]
-    private static void RiderPreferencesItem()
+    [SettingsProvider]
+    private static SettingsProvider RiderPreferencesItem()
     {
-      EditorGUIUtility.labelWidth = 200f;
-      EditorGUILayout.BeginVertical();
-
-      GUI.enabled = RiderScriptEditor.IsRiderInstallation(CodeEditor.CurrentEditorInstallation);
-
-      GUILayout.BeginVertical();
-      LogEventsCollectorEnabled = EditorGUILayout.Toggle(new GUIContent("Pass Console to Rider:"), LogEventsCollectorEnabled);
-
-        OverrideTargetFrameworkVersionOldMono = EditorGUILayout.Toggle(new GUIContent("Override TargetFrameworkVersion:"), OverrideTargetFrameworkVersionOldMono);
-        if (OverrideTargetFrameworkVersionOldMono)
+      var provider = new SettingsProvider("Preferences/Rider", SettingsScope.User)
+      {
+        label = "Rider",
+        keywords = new[] { "Rider" },
+        guiHandler = (searchContext) =>
         {
-          var helpOldMono = @"TargetFramework = 3.5 is recommended.
+          EditorGUIUtility.labelWidth = 200f;
+          EditorGUILayout.BeginVertical();
+
+          GUI.enabled = RiderScriptEditor.IsRiderInstallation(CodeEditor.CurrentEditorInstallation);
+
+          GUILayout.BeginVertical();
+          LogEventsCollectorEnabled =
+            EditorGUILayout.Toggle(new GUIContent("Pass Console to Rider:"), LogEventsCollectorEnabled);
+
+          OverrideTargetFrameworkVersionOldMono =
+            EditorGUILayout.Toggle(new GUIContent("Override TargetFrameworkVersion:"),
+              OverrideTargetFrameworkVersionOldMono);
+          if (OverrideTargetFrameworkVersionOldMono)
+          {
+            var helpOldMono = @"TargetFramework = 3.5 is recommended.
  - With 4.5 Rider may show ambiguous references in UniRx.";
 
-          TargetFrameworkVersionOldMono =
-            EditorGUILayout.TextField(
-              new GUIContent("For Active profile NET 3.5:",
-                helpOldMono), TargetFrameworkVersionOldMono);
-          EditorGUILayout.HelpBox(helpOldMono, MessageType.None);
-        }
+            TargetFrameworkVersionOldMono =
+              EditorGUILayout.TextField(
+                new GUIContent("For Active profile NET 3.5:",
+                  helpOldMono), TargetFrameworkVersionOldMono);
+            EditorGUILayout.HelpBox(helpOldMono, MessageType.None);
+          }
 
-        // Unity 2018.1 doesn't require installed dotnet framework, it references everything from Unity installation
-      if (SystemInfo.operatingSystemFamily == OperatingSystemFamily.Windows && UnityUtils.UnityVersion < new Version(2018, 1))
-      {
-        var detectedDotnetText = string.Empty;
-        var installedFrameworks = GetInstalledNetFrameworks();
-        if (installedFrameworks.Any())
-          detectedDotnetText = installedFrameworks.OrderBy(v => new Version(v)).Aggregate((a, b) => a+"; "+b);
-        EditorGUILayout.HelpBox($"Installed dotnet versions: {detectedDotnetText}", MessageType.None);
-      }
+          // Unity 2018.1 doesn't require installed dotnet framework, it references everything from Unity installation
+          if (SystemInfo.operatingSystemFamily == OperatingSystemFamily.Windows &&
+              UnityUtils.UnityVersion < new Version(2018, 1))
+          {
+            var detectedDotnetText = string.Empty;
+            var installedFrameworks = GetInstalledNetFrameworks();
+            if (installedFrameworks.Any())
+              detectedDotnetText = installedFrameworks.OrderBy(v => new Version(v)).Aggregate((a, b) => a + "; " + b);
+            EditorGUILayout.HelpBox($"Installed dotnet versions: {detectedDotnetText}", MessageType.None);
+          }
 
-      GUILayout.EndVertical();
+          GUILayout.EndVertical();
 
-      OverrideLangVersion = EditorGUILayout.Toggle(new GUIContent("Override LangVersion:"), OverrideLangVersion);
-      if (OverrideLangVersion)
-      {
-        var workaroundUrl = "https://gist.github.com/van800/875ce55eaf88d65b105d010d7b38a8d4";
-        var workaroundText = "Use this <color=#0000FF>workaround</color> if overriding doesn't work.";
-        var helpLangVersion = @"Avoid overriding, unless there is no particular need.";
+          OverrideLangVersion = EditorGUILayout.Toggle(new GUIContent("Override LangVersion:"), OverrideLangVersion);
+          if (OverrideLangVersion)
+          {
+            var workaroundUrl = "https://gist.github.com/van800/875ce55eaf88d65b105d010d7b38a8d4";
+            var workaroundText = "Use this <color=#0000FF>workaround</color> if overriding doesn't work.";
+            var helpLangVersion = @"Avoid overriding, unless there is no particular need.";
 
-        LangVersion =
-          EditorGUILayout.TextField(
-            new GUIContent("LangVersion:",
-              helpLangVersion), LangVersion);
-        LinkButton(caption: workaroundText, url: workaroundUrl);
-        EditorGUILayout.HelpBox(helpLangVersion, MessageType.None);
-      }
-      GUILayout.Label("");
+            LangVersion =
+              EditorGUILayout.TextField(
+                new GUIContent("LangVersion:",
+                  helpLangVersion), LangVersion);
+            LinkButton(caption: workaroundText, url: workaroundUrl);
+            EditorGUILayout.HelpBox(helpLangVersion, MessageType.None);
+          }
 
-      if (!string.IsNullOrEmpty(EditorPluginInterop.LogPath))
-      {
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.PrefixLabel("Log file:");
-        var previous = GUI.enabled;
-        GUI.enabled = previous && SelectedLoggingLevel != LoggingLevel.OFF;
-        var button = GUILayout.Button(new GUIContent("Open log"));
-        if (button)
-        {
-          //UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(PluginEntryPoint.LogPath, 0);
-          // works much faster than the commented code, when Rider is already started
-          CodeEditor.CurrentEditor.OpenProject(EditorPluginInterop.LogPath, 0, 0);
-        }
-        GUI.enabled = previous;
-        GUILayout.EndHorizontal();        
-      }
-      
-      var loggingMsg =
-        @"Sets the amount of Rider Debug output. If you are about to report an issue, please select Verbose logging level and attach Unity console output to the issue.";
-      SelectedLoggingLevel =
-        (LoggingLevel) EditorGUILayout.EnumPopup(new GUIContent("Logging Level:", loggingMsg),
-          SelectedLoggingLevel);
+          GUILayout.Label("");
 
-      
-      EditorGUILayout.HelpBox(loggingMsg, MessageType.None);
-      
-      var githubRepo = "https://github.com/JetBrains/resharper-unity";
-      var caption = $"<color=#0000FF>{githubRepo}</color>";
-      LinkButton(caption: caption, url: githubRepo);
-      
-      GUILayout.FlexibleSpace();
-      GUILayout.BeginHorizontal();
-      
-      GUILayout.FlexibleSpace();
-      var version = Assembly.GetExecutingAssembly().GetName().Version;
-      GUILayout.Label("Plugin version: " + version, ourVersionInfoStyle);
-      
-      GUILayout.EndHorizontal();
-      
-      // left for testing purposes
+          if (!string.IsNullOrEmpty(EditorPluginInterop.LogPath))
+          {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PrefixLabel("Log file:");
+            var previous = GUI.enabled;
+            GUI.enabled = previous && SelectedLoggingLevel != LoggingLevel.OFF;
+            var button = GUILayout.Button(new GUIContent("Open log"));
+            if (button)
+            {
+              //UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(PluginEntryPoint.LogPath, 0);
+              // works much faster than the commented code, when Rider is already started
+              CodeEditor.CurrentEditor.OpenProject(EditorPluginInterop.LogPath, 0, 0);
+            }
+
+            GUI.enabled = previous;
+            GUILayout.EndHorizontal();
+          }
+
+          var loggingMsg =
+            @"Sets the amount of Rider Debug output. If you are about to report an issue, please select Verbose logging level and attach Unity console output to the issue.";
+          SelectedLoggingLevel =
+            (LoggingLevel) EditorGUILayout.EnumPopup(new GUIContent("Logging Level:", loggingMsg),
+              SelectedLoggingLevel);
+
+
+          EditorGUILayout.HelpBox(loggingMsg, MessageType.None);
+
+          var githubRepo = "https://github.com/JetBrains/resharper-unity";
+          var caption = $"<color=#0000FF>{githubRepo}</color>";
+          LinkButton(caption: caption, url: githubRepo);
+
+          GUILayout.FlexibleSpace();
+          GUILayout.BeginHorizontal();
+
+          GUILayout.FlexibleSpace();
+          var version = Assembly.GetExecutingAssembly().GetName().Version;
+          GUILayout.Label("Plugin version: " + version, ourVersionInfoStyle);
+
+          GUILayout.EndHorizontal();
+
+          // left for testing purposes
 /*      if (GUILayout.Button("reset RiderInitializedOnce = false"))
       {
         RiderInitializedOnce = false;
       }*/
 
-      EditorGUILayout.EndVertical();
+          EditorGUILayout.EndVertical();
+        }
+      };
+      return provider;
     }
 
     private static void LinkButton(string caption, string url)
