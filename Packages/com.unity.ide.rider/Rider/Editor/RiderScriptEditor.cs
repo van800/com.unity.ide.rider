266912e554168fea072f8eeafca0f618b338408d
Ivan.Shakhov <van800@gmail.com>
2019-06-16 12:42:20 +0200

    avoid full Sync, when non sln/csprj was changed

--- Packages/com.unity.ide.rider/Rider/Editor/RiderScriptEditor.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/RiderScriptEditor.cs
@@ -40,12 +40,40 @@ namespace Packages.Rider.Editor
           {
             editor.m_Initiliazer.Initialize(path);
           }
+
+          InitProjectFilesWatcher();
         }
       }
       catch (Exception e)
       {
         Debug.LogException(e);
       }
+    }
+
+    private static void InitProjectFilesWatcher()
+    {
+      var watcher = new FileSystemWatcher();
+      watcher.Path = Directory.GetCurrentDirectory();
+      watcher.NotifyFilter = NotifyFilters.LastWrite; //Watch for changes in LastWrite times
+      watcher.Filter = "*.*";
+
+      // Add event handlers.
+      watcher.Changed += OnChanged;
+      watcher.Created += OnChanged;
+
+      watcher.EnableRaisingEvents = true; // Begin watching.
+      
+      AppDomain.CurrentDomain.DomainUnload += (EventHandler) ((_, __) =>
+      {
+        watcher.Dispose();
+      });
+    }
+
+    private static void OnChanged(object sender, FileSystemEventArgs e)
+    {
+      var extension = Path.GetExtension(e.FullPath);
+      if (extension == ".sln" || extension == ".csproj") 
+        RiderScriptEditorData.instance.HasChanges = true;
     }
 
     private static string GetEditorRealPath(string path)
@@ -150,7 +178,11 @@ namespace Packages.Rider.Editor
     public void SyncAll()
     {
       AssetDatabase.Refresh();
-      m_ProjectGeneration.Sync();
+      if (RiderScriptEditorData.instance.HasChanges)
+      {
+        m_ProjectGeneration.Sync();
+        RiderScriptEditorData.instance.HasChanges = false;
+      }
     }
 
     public void Initialize(string editorInstallationPath) // is called each time ExternalEditor is changed
