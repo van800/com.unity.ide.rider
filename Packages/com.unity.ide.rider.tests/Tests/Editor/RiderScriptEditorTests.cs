39959c5a391e85452ae4d5394d12a6a8b32a9a7f
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-06-17 11:28:43 +0200

    add tests with testdata

---
+++ Packages/com.unity.ide.rider.tests/Tests/Editor/RiderScriptEditorTests.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using NUnit.Framework;
+using Packages.Rider.Editor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Packages.Rider.Tests.Editor
+{
+    [TestFixture]
+    public class RiderScriptEditorTests
+    {
+        [TestCase("/Applications/Rider EAP.app")]
+        [TestCase("/Applications/Rider.app")]
+        [TestCase("/Applications/Rider 2017.2.1.app")]
+        [UnityPlatform(RuntimePlatform.OSXEditor)]
+        public void MacIsRiderInstallationTest(string path)
+        {
+            Assert.IsTrue(RiderScriptEditor.IsRiderInstallation(path));
+        }
+        
+        [TestCase(@"C:\Program Files\Rider\bin\rider.exe")]
+        [TestCase(@"C:\Program Files\Rider\bin\rider32.exe")]
+        [TestCase(@"C:\Program Files\Rider\bin\rider64.exe")]
+        [UnityPlatform(RuntimePlatform.WindowsEditor)]
+        public void WindowsIsRiderInstallationTest(string path)
+        {
+            Assert.IsTrue(RiderScriptEditor.IsRiderInstallation(path));
+        }
+        
+        [TestCase("/home/thatguy/Rider/bin/rider.sh")]
+        [UnityPlatform(RuntimePlatform.LinuxEditor)]
+        public void LinuxIsRiderInstallationTest(string path)
+        {
+            Assert.IsTrue(RiderScriptEditor.IsRiderInstallation(path));
+        }
+    }
+}
