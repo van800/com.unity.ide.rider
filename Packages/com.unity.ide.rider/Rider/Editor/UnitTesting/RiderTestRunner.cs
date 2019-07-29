5b527821e70ec2f2ea666195e91022fc4ab94de4
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-07-29 15:35:21 +0200

    avoid multiple tests callback registration

--- Packages/com.unity.ide.rider/Rider/Editor/UnitTesting/RiderTestRunner.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/UnitTesting/RiderTestRunner.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEditor.TestTools.TestRunner.Api;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ namespace Packages.Rider.Editor.UnitTesting
 {
   public static class RiderTestRunner
   {
+    private static readonly TestsCallback Callback = ScriptableObject.CreateInstance<TestsCallback>();
+    
     [UsedImplicitly]
     public static void RunTests(int testMode, string[] assemblyNames, string[] testNames, string[] categoryNames, string[] groupNames)
     {
@@ -20,15 +23,15 @@ namespace Packages.Rider.Editor.UnitTesting
          assemblyNames = assemblyNames,
          testNames = testNames,
          categoryNames = categoryNames,
-         groupNames = groupNames
+         groupNames = groupNames 
+         //, targetPlatform =BuildTarget.StandaloneWindows64 // This can be used to set the target platform when running playmode tests.
+         //, testMode = TestMode.PlayMode
         }
         }
-//                , targetPlatform =
-//                    BuildTarget
-//                        .StandaloneWindows64, // This can be used to set the target platform when running playmode tests.
       });
-
-      api.RegisterCallbacks(ScriptableObject.CreateInstance<TestsCallback>()); // This can be used to receive information about when the test suite and individual tests starts and stops. Provide this with a scriptable object implementing ICallbacks
+      
+      api.UnregisterCallbacks(Callback); // avoid multiple registrations
+      api.RegisterCallbacks(Callback); // This can be used to receive information about when the test suite and individual tests starts and stops. Provide this with a scriptable object implementing ICallbacks
     }
   }
 }
