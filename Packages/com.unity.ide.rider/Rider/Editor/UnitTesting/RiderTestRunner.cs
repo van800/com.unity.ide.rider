65ca72e467d59c70e1d3b04ef58c6b332d1e9555
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2019-08-01 09:42:56 +0200

    api for future use - running both Edit and Play together

--- Packages/com.unity.ide.rider/Rider/Editor/UnitTesting/RiderTestRunner.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/UnitTesting/RiderTestRunner.cs
@@ -10,25 +10,29 @@ namespace Packages.Rider.Editor.UnitTesting
     private static readonly TestsCallback Callback = ScriptableObject.CreateInstance<TestsCallback>();
     
     [UsedImplicitly]
-    public static void RunTests(int testMode, string[] assemblyNames, string[] testNames, string[] categoryNames, string[] groupNames, int? buildTarget)
+    public static void RunTests(int? testMode, string[] assemblyNames, string[] testNames, string[] categoryNames, string[] groupNames, int? buildTarget)
     {
       CallbackData.instance.isRider = true;
             
       var api = ScriptableObject.CreateInstance<TestRunnerApi>();
-      api.Execute(new ExecutionSettings()
+      var mode =(TestMode?)testMode; // for future use, when test-framework allows running Edit and Play at once
+      var settings = new ExecutionSettings();
+      var filter = new Filter
       {
-        filters = new []{
-        new Filter()
-        {
-         assemblyNames = assemblyNames,
-         testNames = testNames,
-         categoryNames = categoryNames,
-         groupNames = groupNames 
-         , targetPlatform = (BuildTarget?)buildTarget // This can be used to set the target platform when running playmode tests.
-         , testMode = (TestMode)testMode
-        }
-        }
-      });
+        assemblyNames = assemblyNames,
+        testNames = testNames,
+        categoryNames = categoryNames,
+        groupNames = groupNames,
+        targetPlatform = (BuildTarget?) buildTarget
+      };
+      
+      if (mode != null)
+        filter.testMode = (TestMode) mode;
+      
+      settings.filters = new []{
+        filter
+      };
+      api.Execute(settings);
       
       api.UnregisterCallbacks(Callback); // avoid multiple registrations
       api.RegisterCallbacks(Callback); // This can be used to receive information about when the test suite and individual tests starts and stops. Provide this with a scriptable object implementing ICallbacks
