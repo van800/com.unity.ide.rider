10fe4ddad24790ccecfaff9ca856f59158c3fced
nicklas <miniwolf1508@gmail.com>
2019-11-01 12:51:28 +0100

    Test player and editor assemblies are loaded collected Test player assemblies are renamed to "name-player" Test editor and player assemblies receive UnityEngine reference from CompilationPipeline Test editor assemblies receive UnityEditor reference from CompilationPipeline

---
+++ Packages/com.unity.ide.rider.tests/Tests/Editor/AssemblyNameProviderTests.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using NUnit.Framework;
+using Packages.Rider.Editor.ProjectGeneration;
+using UnityEditor.Compilation;
+
+namespace Packages.Rider.Editor.Tests
+{
+    public class AssemblyNameProviderTests
+    {
+        AssemblyNameProvider m_AssemblyNameProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_AssemblyNameProvider = new AssemblyNameProvider();
+        }
+
+        [Test]
+        public void AllEditorAssemblies_AreCollected()
+        {
+            var editorAssemblies = CompilationPipeline.GetAssemblies(AssembliesType.Editor);
+
+            var collectedAssemblies = m_AssemblyNameProvider.GetAssemblies(s => true).ToList();
+
+            foreach (Assembly editorAssembly in editorAssemblies)
+            {
+                Assert.IsTrue(collectedAssemblies.Any(assembly => assembly.name == editorAssembly.name && assembly.outputPath == editorAssembly.outputPath), $"{editorAssembly.name}: was not found in collection.");
+            }
+        }
+
+        [Test]
+        public void AllEditorAssemblies_HaveAReferenceToUnityEditorAndUnityEngine()
+        {
+            var editorAssemblies = CompilationPipeline.GetAssemblies(AssembliesType.Editor);
+
+            foreach (Assembly editorAssembly in editorAssemblies)
+            {
+                Assert.IsTrue(editorAssembly.allReferences.Any(reference => reference.EndsWith("UnityEngine.dll")));
+                Assert.IsTrue(editorAssembly.allReferences.Any(reference => reference.EndsWith("UnityEditor.dll")));
+            }
+        }
+
+        [Test]
+        public void AllPlayerAssemblies_AreCollected()
+        {
+            var playerAssemblies = CompilationPipeline.GetAssemblies(AssembliesType.Player);
+
+            var collectedAssemblies = m_AssemblyNameProvider.GetAssemblies(s => true).ToList();
+
+            foreach (Assembly playerAssembly in playerAssemblies)
+            {
+                Assert.IsTrue(collectedAssemblies.Any(assembly => assembly.name == playerAssembly.name + "-player" && assembly.outputPath == playerAssembly.outputPath), $"{playerAssembly.name}: was not found in collection.");
+            }
+        }
+
+        [Test]
+        public void AllPlayerAssemblies_HaveAReferenceToUnityEngine()
+        {
+            var playerAssemblies = CompilationPipeline.GetAssemblies(AssembliesType.Player);
+
+            foreach (Assembly playerAssembly in playerAssemblies)
+            {
+                Assert.IsTrue(playerAssembly.allReferences.Any(reference => reference.EndsWith("UnityEngine.dll")));
+                Assert.IsFalse(playerAssembly.allReferences.Any(reference => reference.EndsWith("UnityEditor.dll")));
+            }
+        }
+    }
+}
