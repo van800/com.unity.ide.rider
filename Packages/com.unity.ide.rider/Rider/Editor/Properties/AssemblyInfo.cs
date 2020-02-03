a34da16d9a9cdda85011b32f6a4cef48def9c520
Ivan.Shakhov <van800@gmail.com>
2020-02-03 09:16:50 +0100

    add InternalsVisibleTo

---
+++ Packages/com.unity.ide.rider/Rider/Editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Unity.Rider.Editor")]
+[assembly: InternalsVisibleTo("Unity.Rider.EditorTests")]
+[assembly: InternalsVisibleTo("Unity.PackageValidationSuite.Editor")]
+
+[assembly: AssemblyVersion("1.0.0")]
