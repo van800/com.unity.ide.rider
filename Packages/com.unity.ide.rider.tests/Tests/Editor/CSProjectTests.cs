9ad9e80ef69f03528ea08479f5a66c315e106a17
Miao Yu Goh <57358879+MiaoYu-Goh@users.noreply.github.com>
2020-02-13 15:38:47 +0100

    Update CSProjectTests.cs

--- Packages/com.unity.ide.rider.tests/Tests/Editor/CSProjectTests.cs
+++ Packages/com.unity.ide.rider.tests/Tests/Editor/CSProjectTests.cs
@@ -725,12 +725,14 @@ namespace Packages.Rider.Editor.Tests
             [Test]
             public void RoslynAnalyzerRulesetFiles_WillBeIncluded()
             {
+                #if UNITY_EDITOR_2020_2_OR_NEWER
                 var roslynAnalyzerRuleSetPaths = new[] {"Assets/RoslynRuleSet.ruleset"};
                 
                 m_Builder.WithAssemblyData(files: new[] {"file.cs"}, roslynAnalyzerRulesetPaths: roslynAnalyzerRuleSetPaths).Build().Sync();
                 var csProjectFileContents = m_Builder.ReadProjectFile(m_Builder.Assembly);
                 XmlDocument csProjectXmlFile = XMLUtilities.FromText(csProjectFileContents);
                 XMLUtilities.AssertAnalyzerRuleSetsMatchExactly(csProjectXmlFile, roslynAnalyzerRuleSetPaths);
+                #endif
             }
             
             [Test]
