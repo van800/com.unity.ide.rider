6d7844bfa1d5bbabcbb54754b48b9d4026f85470
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-11-23 10:35:09 +0100

    3.0.4

--- Packages/com.unity.ide.rider/Rider/Editor/Properties/AssemblyInfo.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/Properties/AssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Assembly-CSharp-Editor")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
-[assembly: AssemblyVersion("3.0.1")]
+[assembly: AssemblyVersion("3.0.4")]
