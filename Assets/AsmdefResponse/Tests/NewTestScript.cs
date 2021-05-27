5a10e9e1c09df818c4fec02b854d4793b42f4087
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-05-27 17:57:02 +0200

    PlayMode tests

---
+++ Assets/AsmdefResponse/Tests/NewTestScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
