773245e0366d2881fd7764c5a4a3ee9f702c674b
Ivan.Shakhov <van800@gmail.com>
2022-03-13 10:57:26 +0100

    merge from 3.0.13, which adds an option to disable checking lastwrite

--- Packages/com.unity.ide.rider/Rider/Editor/ProjectGeneration/LastWriteTracker.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/ProjectGeneration/LastWriteTracker.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using UnityEngine;
 
 namespace Packages.Rider.Editor.ProjectGeneration
 {
@@ -9,12 +10,20 @@ namespace Packages.Rider.Editor.ProjectGeneration
     {
         internal static bool HasLastWriteTimeChanged()
         {
+          if (!PluginSettings.TrackProjectFileChanges)
+          {
+            if (PluginSettings.SelectedLoggingLevel >= LoggingLevel.VERBOSE)
+              Debug.Log("Project files tracking is disabled.");
+            return false;
+          }
+          
             // any external changes of sln/csproj or manifest.json should cause their regeneration
-            // Directory.GetCurrentDirectory(), "*.csproj", "*.sln", "Packages/manifest.json"
+            // Directory.GetCurrentDirectory(), "*.csproj", "*.sln"
             var files = new List<FileInfo>();
-            
-            files.AddRange(new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles("*.csproj"));
-            files.AddRange(new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles("*.sln"));
+
+            var directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            files.AddRange(directoryInfo.GetFiles("*.csproj"));
+            files.Add(new FileInfo(Path.Combine(directoryInfo.FullName, directoryInfo.Name+".sln")));
 
             return files.Any(a => a.LastWriteTime > RiderScriptEditorPersistedState.instance.LastWrite);
         }
@@ -24,10 +33,10 @@ namespace Packages.Rider.Editor.ProjectGeneration
             var fileInfo = new FileInfo(path);
             if (fileInfo.Directory == null)
                 return;
-            
-            if (fileInfo.Directory.FullName.Equals(new DirectoryInfo(Directory.GetCurrentDirectory()).FullName, StringComparison.OrdinalIgnoreCase) &&
+            var directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            if (fileInfo.Directory.FullName.Equals(directoryInfo.FullName, StringComparison.OrdinalIgnoreCase) &&
                 (fileInfo.Extension.Equals(".csproj", StringComparison.OrdinalIgnoreCase)
-                 || fileInfo.Extension.Equals(".sln", StringComparison.OrdinalIgnoreCase)))
+                 || fileInfo.Name.Equals(directoryInfo.Name + ".sln", StringComparison.OrdinalIgnoreCase)))
             {
                 RiderScriptEditorPersistedState.instance.LastWrite = fileInfo.LastWriteTime;
             }
