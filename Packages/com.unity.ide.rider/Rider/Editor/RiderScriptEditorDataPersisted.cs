cbd89e2db1f44d9eab65a0547632b48becd1c740
Ivan.Shakhov <van800@gmail.com>
2022-03-30 21:01:03 +0200

    Move Rider package persisted state to Library, to avoid vcs collisions or adding it specifically to gitignore

--- Packages/com.unity.ide.rider/Rider/Editor/RiderScriptEditorDataPersisted.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/RiderScriptEditorDataPersisted.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace Packages.Rider.Editor
 {
 #if UNITY_2020_1_OR_NEWER // API doesn't exist in 2019.4
-  [FilePath("ProjectSettings/RiderScriptEditorPersistedState.asset", FilePathAttribute.Location.ProjectFolder)]
+  [FilePath("Library/com.unity.ide.rider/PersistedState.asset", FilePathAttribute.Location.ProjectFolder)]
 #endif
   internal class RiderScriptEditorPersistedState : ScriptableSingleton<RiderScriptEditorPersistedState>
   {
