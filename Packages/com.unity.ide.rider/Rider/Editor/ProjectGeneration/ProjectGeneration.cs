b578c0711ceb0a913ea34e1d0197f7569eafbce6
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2023-01-02 15:02:10 +0100

    avoid doing ProjectGeneration twice on the first start-up

--- Packages/com.unity.ide.rider/Rider/Editor/ProjectGeneration/ProjectGeneration.cs
+++ Packages/com.unity.ide.rider/Rider/Editor/ProjectGeneration/ProjectGeneration.cs
@@ -113,8 +113,6 @@ namespace Packages.Rider.Editor.ProjectGeneration
                                                                || (checkProjectFiles && LastWriteTracker.HasLastWriteTimeChanged()))
       {
         Sync();
-        RiderScriptEditorData.instance.hasChanges = false;
-        RiderScriptEditorData.instance.InvalidateSavedCompilationDefines();
         return true;
       }
 
@@ -147,6 +145,8 @@ namespace Packages.Rider.Editor.ProjectGeneration
       OnGeneratedCSProjectFiles(types);
       m_AssemblyNameProvider.ResetPackageInfoCache();
       m_AssemblyNameProvider.ResetAssembliesCache();
+      RiderScriptEditorData.instance.hasChanges = false;
+      RiderScriptEditorData.instance.InvalidateSavedCompilationDefines();
     }
 
     public bool HasSolutionBeenGenerated()
