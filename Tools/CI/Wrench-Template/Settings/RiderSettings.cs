836f0b66c3875c89fac90748f4b854773997ad1d
eleonoraGirardini <eleonora.girardini@unity3d.com>
2024-10-08 17:35:23 +0200

    Set up wrench

---
+++ Tools/CI/Wrench-Template/Settings/RiderSettings.cs
@@ -0,0 +1,30 @@
+﻿using RecipeEngine.Api.Settings;
+using RecipeEngine.Modules.Wrench.Models;
+using RecipeEngine.Modules.Wrench.Settings;
+
+namespace Rider.Cookbook.Settings;
+
+public class RiderSettings : AnnotatedSettingsBase
+{
+    // Path from the root of the repository where packages are located.
+    readonly string[] PackagesRootPaths = {"Packages/"};
+
+    // update this to list all packages in this repo that you want to release.
+    Dictionary<string, PackageOptions> PackageOptions = new()
+    {
+        {
+            "com.unity.ide.rider",
+            new PackageOptions() { ReleaseOptions = new ReleaseOptions() { IsReleasing = true } }
+        }
+    };
+
+    public RiderSettings()
+    {
+        Wrench = new WrenchSettings(
+            PackagesRootPaths,
+            PackageOptions
+        );      
+    }
+
+    public WrenchSettings Wrench { get; private set; }
+}
