f69e60375fd68b204b05eecc4266acb473c88075
Darius Skergelza <darius.skergelza@unity3d.com>
2025-03-12 15:40:10 +0100

    Regenerate CI to use Ubuntu 18.04

--- Tools/CI/Settings/RiderSettings.cs
+++ Tools/CI/Settings/RiderSettings.cs
@@ -27,8 +27,17 @@ public class RiderSettings : AnnotatedSettingsBase
     private readonly Dictionary<SystemType, Platform> ImageOverrides = new()
     {
         {
+            SystemType.Windows,
+            new Platform(new Agent("package-ci/win10:default", FlavorType.BuildLarge, ResourceType.Vm), SystemType.Windows)
+        },
+        {
+            SystemType.MacOS,
+            new Platform(new Agent("package-ci/macos-13:default", FlavorType.BuildExtraLarge, ResourceType.VmOsx),
+                SystemType.MacOS)
+        },
+        {
             SystemType.Ubuntu,
-            new Platform(new Agent("package-ci/ubuntu-20.04:default", FlavorType.BuildLarge, ResourceType.Vm),
+            new Platform(new Agent("package-ci/ubuntu-18.04:default", FlavorType.BuildLarge, ResourceType.Vm),
                 SystemType.Ubuntu)
         }
     };
@@ -39,6 +48,8 @@ public class RiderSettings : AnnotatedSettingsBase
             PackagesRootPaths,
             PackageOptions
         );      
+
+        Wrench.Packages["com.unity.ide.rider"].EditorPlatforms = ImageOverrides;
     }
 
     public WrenchSettings Wrench { get; private set; }
