5a429cb578df18a7d753bb630bd472ba5325f9df
Darius Skergelza <darius.skergelza@unity3d.com>
2025-09-03 09:41:48 +0200

    Updated jobs generated by wrench and changed ubuntu version to be supported by 2019.4 stream

--- Tools/CI/Settings/RiderSettings.cs
+++ Tools/CI/Settings/RiderSettings.cs
@@ -22,13 +22,33 @@ public class RiderSettings : AnnotatedSettingsBase
             new PackageOptions() { ReleaseOptions = new ReleaseOptions() { IsReleasing = true } }
         }
     };
+    
+    private readonly Dictionary<SystemType, Platform> ImageOverrides = new()
+    {
+        {
+            SystemType.Windows,
+            new Platform(new Agent("package-ci/win10:default", FlavorType.BuildLarge, ResourceType.Vm), SystemType.Windows)
+        },
+        {
+            SystemType.MacOS,
+            new Platform(new Agent("package-ci/macos-13:default", FlavorType.BuildExtraLarge, ResourceType.VmOsx),
+                SystemType.MacOS)
+        },
+        {
+            SystemType.Ubuntu,
+            new Platform(new Agent("package-ci/ubuntu-18.04:v4", FlavorType.BuildLarge, ResourceType.Vm),
+                SystemType.Ubuntu)
+        }
+    };
 
     public RiderSettings()
     {
         Wrench = new WrenchSettings(
             PackagesRootPaths,
             PackageOptions
-        );      
+        );
+
+         Wrench.Packages["com.unity.ide.rider"].EditorPlatforms = ImageOverrides;
     }
 
     public WrenchSettings Wrench { get; private set; }
